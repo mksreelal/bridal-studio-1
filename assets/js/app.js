585cd07215e1bc978704906df3a2d03bcd8faebc
musadhiq mp <musadhiqmp@gmail.com>
2021-08-09 15:08:13 +0530

    finalyze

---
+++ assets/js/app.js
@@ -0,0 +1,55 @@
+window.addEventListener("load", function (e) {
+  const humburger = document.querySelector(".humburgar");
+  const navlinks = document.querySelector(".nav-links");
+  const links = document.querySelectorAll(".link");
+  const overlay = document.querySelector(".overlay");
+  const message = document.getElementById("message");
+  const button = document.querySelector(".msg");
+  //   fetch("https://github.com/musadhiq/musadhiq/blob/main/gallerySrc.json")
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       console.log(data);
+  //     });
+  //   nav
+  humburger.addEventListener("click", (e) => {
+    navlinks.classList.toggle("open");
+    overlay.classList.toggle("open");
+    links.forEach((link) => {
+      link.addEventListener("click", () => {
+        navlinks.classList.remove("open");
+        overlay.classList.remove("open");
+      });
+    });
+  });
+  overlay.addEventListener("click", (e) => {
+    navlinks.classList.remove("open");
+    overlay.classList.remove("open");
+  });
+
+  button.addEventListener("click", () => {
+    let messageVal = message.value;
+    button.setAttribute(
+      "href",
+      "https://wa.me/+917558936055?text=" + messageVal
+    );
+  });
+
+  // Initialize and add the map
+  function initMap() {
+    // The location of Uluru
+    const uluru = { lat: -25.344, lng: 131.036 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
+  initMap();
+});
